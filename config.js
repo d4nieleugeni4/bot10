e3915c083572ae2dd0db15d46f32a597c27fa72d
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-12-30 07:04:25 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+export default {
+  bot: {
+    nome: "Bot Ping",
+    numero: "5511963546094" // número do bot (opcional, só informativo)
+  },
+
+  dono: {
+    numero: "5524981321901"
+  },
+
+  prefixo: "." 
+  // se deixar "" ou null → funciona sem prefixo
+}
