1c9e8e5e8b5467eac00ec969d2cc0d2751746d49
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-12-30 07:11:32 -0300

    Refactor ping command to use ES6 module syntax

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,3 +1,11 @@
-module.exports = async (sock, from) => {
-  await sock.sendMessage(from, { text: "🏓 Pong!" });
+export default {
+  nome: "ping",
+
+  async executar(sock, msg) {
+    const from = msg.key.remoteJid;
+
+    await sock.sendMessage(from, {
+      text: "🏓 Pong!"
+    });
+  }
 };
